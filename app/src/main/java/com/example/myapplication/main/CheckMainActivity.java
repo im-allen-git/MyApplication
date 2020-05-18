fda7fa0154e7ea911fe916677fa5c843e3a2d63c
Alan <jxnx888@hotmail.com>
2020-05-18 13:33:38 +0800

    test goto webview

--- app/src/main/java/com/example/myapplication/main/CheckMainActivity.java
+++ app/src/main/java/com/example/myapplication/main/CheckMainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.main;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +15,12 @@ public class CheckMainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_main);
+
+        /*setContentView(R.layout.index);
+        WebView webView=(WebView)findViewById(R.id.children);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        //确保跳转到另一个网页时仍然在当前WebView显示
+        webView.loadUrl("http://192.168.1.163:8080/index.html");*/
     }
 }
